f488c2328232b14fd70c20728a6ad8c5883dbdfd
GermanDTorres <166769020+GermanDTorres@users.noreply.github.com>
2024-10-29 23:13:57 -0300

    Hub que use para el signalr en infoOficina

---
+++ BlazorApp/Hubs/OficinasHub.cs
@@ -0,0 +1,10 @@
+﻿using BlazorApp.Models.DTs;
+using Microsoft.AspNetCore.SignalR;
+
+public class OficinasHub : Hub
+{
+    public async Task EnviarOficinasActualizadas(List<DTOficina> oficinas)
+    {
+        await Clients.All.SendAsync("ReceiveOficinas", oficinas);
+    }
+}
