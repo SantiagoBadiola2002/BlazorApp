66e4ced46c2b755d083a5d32d72ecda898c316b1
GermanDTorres <166769020+GermanDTorres@users.noreply.github.com>
2024-11-02 20:37:20 -0300

    Update DTAtencionCliente.cs

--- BlazorApp/Models/DTs/DTAtencionCliente.cs
+++ BlazorApp/Models/DTs/DTAtencionCliente.cs
@@ -1,4 +1,4 @@
-﻿namespace BlazorApp.Models.DTs
+namespace BlazorApp.Models.DTs
 {
     public class DTAtencionCliente
     {
@@ -8,6 +8,14 @@
         public int OficinaId { get; set; }
         public DateTime Fecha { get; set; }
         public int Duracion { get; set; } // Duración en minutos
+
+        public int Mes => Fecha.Month;        // Número del mes (1-12) para ordenar
+        public string NombreMes => $"{Fecha.ToString("MMMM")} {Fecha.Year}"; // Nombre del mes y el año
+
+        public string NombreOperario { get; set; } 
+        public string NombreOficina { get; set; }
+
+
     }
 }
 
